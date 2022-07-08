9f25a954044d07f4c5cfb58af395e3cfedbbb5b5
Kavishka Sulakshana <kss20001218@gmail.com>
2022-07-09 03:34:37 +0530

    update

--- Labsheet2/Q4b.scala
+++ Labsheet2/Q4b.scala
@@ -1,4 +1,18 @@
-def attendees(ticket:Int):Int = 120 + ((15-ticket)/5)*20
-def totRev(ticket:Int):Int = attendees(ticket)*ticket
-def cost(ticket:Int):Int = 500+3*attendees(ticket)
-def profit(ticket:Int):Int = totRev(ticket)-cost(ticket)
+object Q4b{
+    def main(args:Array[String])={
+        var x = 0
+        var max,maxT = 0
+        for(x <- 0 to 40; if x%5 == 0){
+            if(profit(x)>max){
+                max = profit(x)
+                maxT = x
+            }    
+        }
+        println(maxT+" "+max)
+    }
+    def attendees(ticket:Int):Int = 120 + ((15-ticket)/5)*20
+    def totRev(ticket:Int):Int = attendees(ticket)*ticket
+    def cost(ticket:Int):Int = 500+3*attendees(ticket)
+    def profit(ticket:Int):Int = totRev(ticket)-cost(ticket)
+}
+
