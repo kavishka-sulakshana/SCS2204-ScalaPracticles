bb0b36194a8960b3b38b8363ea33a79a5e63ba53
Kavishka Sulakshana <kss20001218@gmail.com>
2022-07-12 19:21:38 +0530

    do all questions in tutorial 3

--- Tutorial 3/Q9.scala
+++ Tutorial 3/Q9.scala
@@ -1,4 +1,5 @@
 import scala.io.StdIn.readLine
+import scala.io.StdIn.readDouble
 
 object Q9 extends App{
     def add(num1:Double, num2:Double):Double = num1 + num2
@@ -10,15 +11,12 @@ object Q9 extends App{
     
     def getInput(no:Int):Double ={
         printf("Enter number %d : ",no)
-        return readLine().toDouble
+        return readDouble()
     } 
-
 
     val num1 = getInput(1)
     val num2 = getInput(2)
     printf("Enter your operator : ")
     val op = readLine()
-    if(op=="+" || op=="-" || op=="*" || op=="/") printf("Answer is %.2f\n",calculate(num1,num2,op)) else printf("Invalid operator\n")
-
-
+    if(op=="+" || op=="-" || op=="*" || op=="/") printf("Answer is %.3f\n",calculate(num1,num2,op)) else printf("Invalid operator\n")
 }
