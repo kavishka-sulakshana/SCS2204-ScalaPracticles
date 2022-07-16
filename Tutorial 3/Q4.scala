cdd3d458b8463ca63e4926676edf085fe93eb841
Kavishka Sulakshana <kss20001218@gmail.com>
2022-07-16 13:31:11 +0530

    some update

--- Tutorial 3/Q4.scala
+++ Tutorial 3/Q4.scala
@@ -1,6 +1,6 @@
 object Q4 extends App{
     def discount(p:Double):Double = p*60/100
-    def wholesaleCost(n:Int,p:Double=24.5):Double = if(n<=50) discount(p)*3*n else discount(p)*(3*50+0.75*(n-50))
+    def wholesaleCost(n:Int,p:Double=24.95):Double = if(n<=50) (discount(p)+3)*n else (discount(p)+3)*50+(discount(p)+0.75)*(n-50)
 
     println(wholesaleCost(60))
 }
