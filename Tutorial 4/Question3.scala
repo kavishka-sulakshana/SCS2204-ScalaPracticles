fd23fc6bfadf1ad92aeff10df650ce7f10ec4fd1
Kavishka Sulakshana <kss20001218@gmail.com>
2022-08-01 17:59:02 +0530

    update

--- Tutorial 4/Question3.scala
+++ Tutorial 4/Question3.scala
@@ -1,8 +1,9 @@
 object Question3 extends App{
     def toUpper(str:String):String = str.toUpperCase()
     def toLower(str:String):String = str.toLowerCase()
-    def formatNames(name:String,func:(String) => String):String = func(name)
+    def formatNames(name:String)(func:(String) => String):String = func(name)
     
-    println(formatNames("Benny",toUpper(_)))
+    println(formatNames("Benny")(toUpper(_)))
+    println(formatNames("BEnny")(toLower(_)))
 
 }
