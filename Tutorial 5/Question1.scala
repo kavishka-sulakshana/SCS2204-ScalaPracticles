5eacc2ab059d0680e0fcba4f612f161182815e54
Kavishka Sulakshana <kss20001218@gmail.com>
2022-08-26 10:11:52 +0530

    some update

--- Tutorial 5/Question1.scala
+++ Tutorial 5/Question1.scala
@@ -1,9 +1,10 @@
 object Question1 extends App{
     def isPrime(n:Int, m:Int = 2):Boolean = n match{
+        case x if x<2 => false
         case x if x==m => true
         case x if x%m == 0 => false
         case _ => isPrime(n,m+1) 
     }
 
-    println(isPrime(99))
+    println(isPrime(1))
 }
